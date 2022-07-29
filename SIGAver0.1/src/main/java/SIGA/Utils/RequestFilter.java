97abc2c81003ac753dbdf719429acab38cd584a9
RubenNH <104938082+RubenNH@users.noreply.github.com>
2022-07-28 23:06:26 -0500

    v0.1

---
+++ SIGAver0.1/src/main/java/SIGA/Utils/RequestFilter.java
@@ -0,0 +1,14 @@
+package SIGA.Utils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestFilter{
+
+}
