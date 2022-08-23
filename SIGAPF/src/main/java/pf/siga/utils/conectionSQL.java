75ddb7fce38bca54ea2f5e5daf93f2504b266473
gdav <20193tn142@utez.edu.mx>
2022-08-22 21:00:19 -0500

    Conexión con la BD de PLSQL trabajando en docker, en localhost con el puerto 1521. La BD tiene que ser XE (Si crearon todo lo de docker con los comandos que nos enseñó Carsi no debería de haber problema, ya que con esos se crea en XE). Solo cambien donde dice usuario y contraseña, línea 13 de connectionSQL.java

--- SIGAPF/src/main/java/pf/siga/utils/conectionSQL.java
+++ SIGAPF/src/main/java/pf/siga/utils/conectionSQL.java
@@ -6,18 +6,11 @@ import java.sql.Connection;
 
 public class conectionSQL {
     public Connection getConnection() {
-        final String DBNAME = "SIGA",
-                USERNAME = "root",
-                PASSWORD = "root",
-                TIMEZONE = "America/Mexico_City",
-                USESSL = "false",
-                PUBLICKEY = "true";
-        String dataSource = String.format("jdbc:mysql://localhost:3306/%s?user=%s" +
-                        "&password=%s&serverTimezone=%s&useSSL=%s&allowPublicKeyRetrieval=%s",
-                DBNAME, USERNAME, PASSWORD, TIMEZONE, USESSL, PUBLICKEY);
+
+
         try {
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            return DriverManager.getConnection(dataSource);
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","usuario-docker-bd","contraseña-docker-bd");
         } catch (SQLException e) {
             System.out.println(this.getClass().getCanonicalName() +
                     "->" + e.getMessage());
@@ -32,6 +25,7 @@ public class conectionSQL {
                 System.out.println("Conexión realizada");
                 conn.close();
             } catch (SQLException e) {
+
             }
         } else {
             System.out.println("Conexión fallida");
